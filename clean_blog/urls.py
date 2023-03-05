7314ee63643ad240c27ffb6d1d0c7a4e943e1a19
Shuhib <shuhib_s@live.de>
2023-03-05 11:37:40 +0100

    we add links between two pages

--- clean_blog/urls.py
+++ clean_blog/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('1/', views.first, name='first'),
+    path('2/', views.second, name='second'),
 ]
--- clean_blog/views.py
+++ clean_blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'index.html')
+def first(request):
+    return render(request, 'first.html')
+
+
+def second(request):
+    return render(request, 'second.html')
